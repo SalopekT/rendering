6a883a88d29ad7d12a1c367dd0fff5cc9ea1ba07
Tin Salopek <tin.salopek2@gmail.com>
2025-08-06 00:38:10 +0200

    new classes

--- Rendering/main.cpp
+++ Rendering/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 
 int main()
@@ -34,6 +35,11 @@ int main()
     //render loop with double buffering
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -45,4 +51,10 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
