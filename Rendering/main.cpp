58b3e6580c6d56f9e87f21136394957f920a23e3
Tin Salopek <tin.salopek2@gmail.com>
2025-08-09 16:30:50 +0200

    compiling and linking shaders

--- Rendering/main.cpp
+++ Rendering/main.cpp
@@ -4,7 +4,11 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include "Object.hpp"
+#include "Camera.hpp"
 
 
 
@@ -14,13 +18,6 @@ void processInput(GLFWwindow* window);
 
 int main()
 {
-    Object* obj = new Object("C:\\Users\\tinsa\\Projects\\Graphics\\Rendering\\Rendering\\Objects\\teapot.obj");
-    //obj->print();
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(obj->getMesh(0).getVerticesArray()), obj->getMesh(0).getVerticesArray(), GL_STATIC_DRAW);
-    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
@@ -45,6 +42,86 @@ int main()
 
     glViewport(0, 0, 800, 600);
 
+    //camera settings
+    glm::vec3 camPosition(10, 0, 0);
+    glm::vec3 camCenter(0, 0, 0);
+    glm::vec3 camViewUp(0, 1, 0);
+    Camera* cam = new PerspectiveCamera(camPosition, camCenter, camViewUp, 45.0f, 800, 600, 0.01f, 100.0f);
+
+    //object creation
+    Object* obj = new Object("C:\\Users\\tinsa\\Projects\\Graphics\\Rendering\\Rendering\\Objects\\teapot.obj");
+    //obj->print();
+
+    //generating vertex buffer object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(obj->getMesh(0).getVerticesArray()), obj->getMesh(0).getVerticesArray(), GL_STATIC_DRAW);
+
+    //creating a vertex shader object
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    //reading shader source code
+    std::ifstream file("C:\\Users\\tinsa\\Projects\\Graphics\\Rendering\\Rendering\\Rendering\\vertexShader1.vert");
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    std::string shaderSrcCode = buffer.str();
+    const char* shaderSrcCodePtr = shaderSrcCode.c_str();
+
+    //compiling a shader
+    glShaderSource(vertexShader, 1, &shaderSrcCodePtr, NULL);
+    glCompileShader(vertexShader);
+
+    //checking if compiling succeded
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    //creating a fragment shader object
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    //reading shader from source code
+    std::ifstream file2("C:\\Users\\tinsa\\Projects\\Graphics\\Rendering\\Rendering\\Rendering\\fragmentShader1.frag");
+    std::ostringstream buffer2;
+    buffer2 << file2.rdbuf();
+    std::string shaderSrcCode2 = buffer2.str();
+    const char* shaderSrcCodePtr2 = shaderSrcCode2.c_str();
+
+    //compiling a shader
+    glShaderSource(fragmentShader, 1, &shaderSrcCodePtr2, NULL);
+    glCompileShader(fragmentShader);
+
+    //checking if compiling succeded
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    //creating a shader programme
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    //linking shaders
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::LINKING FAILED\n" << infoLog << std::endl;
+    }
+
     //render loop with double buffering
     while (!glfwWindowShouldClose(window))
     {
@@ -59,6 +136,8 @@ int main()
     }
 
     delete obj;
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
     glfwTerminate();
     return 0;
 }
