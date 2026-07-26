f7e751616b42edfad334c8887433a256d8e376e9
Tin Salopek <tin.salopek2@gmail.com>
2026-07-26 23:01:43 +0200

    small addition

--- Rendering/Renderer.cpp
+++ Rendering/Renderer.cpp
@@ -4,10 +4,17 @@ void Renderer::renderScene() {
 	std::vector<std::shared_ptr<Lightning>> lightsInScene = this->scene->getLights();
 	int numLights = lightsInScene.size();
 	std::vector<int> lightTypes;
+	std::vector<glm::vec3> lightPositions;
+	std::vector<glm::mat3> lightIntensities;
+	std::vector<glm::vec3> lightDirections;
 	for (std::shared_ptr<Lightning> light : lightsInScene) {
 		lightTypes.push_back(light->getType());
+		lightPositions.push_back(light->getLightPosition());
+		lightIntensities.push_back(light->getIntensitiesMatrix());
+		lightDirections.push_back(light->);
 	}
-	this->shaderProgramme->setUniformsFragmentShader(numLights,lightTypes.data(),);
+	this->shaderProgramme->setUniformsFragmentShader(numLights,lightTypes.data(),lightPositions.data(),lightIntensities.data(),
+													);
 	for (int i=0;i<this->scene->getObjects().size();i++) {
 		this->shaderProgramme->setUniformsVertexShader(this->scene->getObjects().at(i)->getTransform().getModelMatrix(),
 														this->scene->getCamera()->getLookAtMatrix(),
